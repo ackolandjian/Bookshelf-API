c72fd8e5846ead1ad5d4746658dd05d008fe79a1
ackolandjian <girl_7_who@hotmail.com>
2020-12-04 23:08:14 +0100

    last commit

--- app/src/main/java/com/ismin/android/Book.kt
+++
@@ -1,5 +0,0 @@
-package com.ismin.android
-
-import java.io.Serializable
-
-data class Book(val title: String, val author: String, val date: String) : Serializable
--- app/src/main/java/com/ismin/android/onFavoriteListener.kt
+++
@@ -1,5 +0,0 @@
-package com.ismin.android
-
-interface onFavoriteListener {
-    fun unfavFavorite(index: Int)
-}
